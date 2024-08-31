2039276bb603c65a7d5e34843920738e16ab3928
0xgwyn <ali.dindamal@gmail.com>
2024-08-31 14:48:59 +0330

    Modified Bio

--- views/Bio.vue
+++ views/Bio.vue
@@ -3,7 +3,7 @@
     <img class="w-full h-auto" src="" />
     <h2 class="font-bold text-2xl py-5">Ali Dindamal aka (Gwyn)</h2>
     <h4 class="text-gray-600 font-medium text-xs" style="padding-bottom: 10px">
-        A developer, cybersecurity enthusiast, and Computer Engineering graduate from Shahid Chamran University of Ahvaz
+        A developer, cybersecurity enthusiast, and Computer Engineering graduate from Ahvaz
     </h4>
     <div class="pt-7">
         <h3 class="underline font-bold text-md pb-1">About Me</h3>
@@ -11,7 +11,7 @@
             Hello! I'm a {{ new Date().getFullYear() - 2000 }} year old guy passionate about ethically breaking and securing stuff. Over the past years, I have been dedicated to expanding my skill set in various areas including programming, networking, linux, databases, and web applications and their vulnerabilities. I have successfully completed projects that demonstrate my capabilities that are present in the <b class="font-bold">Projects</b> folder.
         </p>
         <p class="font-thin text-sm pb-2.5">
-            Additionally, I have gained invaluable experience by participating in Bug Bounty programs, engaging in vulnerability discovery and assessment, as well as developing related tools for such tasks. I also have played the role of a Teacher Assistant in the Computer Networks course at my university.
+            Additionally, I have gained invaluable experience by participating in Bug Bounty programs, engaging in vulnerability discovery and assessment, as well as developing related tools for such tasks. I also have played the role of a Teacher Assistant in the Computer Networks course at Shahid Chamran university.
         </p>
         <p class="font-thin text-sm pb-2.5">
             This website serves as a platform to showcase my academic journey, accomplishments, and projects. Feel free to explore and learn more about my educational background and experiences.
