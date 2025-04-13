6bdc06a95986c8208af122b2fc800aa6d3c98e2e
0xgwyn <ali.dindamal@gmail.com>
2025-04-13 20:22:30 +0330

    Stop video from autoplaying

--- views/VideoWindow.vue
+++ views/VideoWindow.vue
@@ -1,19 +1,24 @@
 <template>
-    <!-- <div class="video-container"> -->
-      <iframe class="video-container" src="/files/intro-28.mp4"  allowfullscreen></iframe>
-    <!-- </div> -->
-  </template>
-  
-  <script setup>
-  // No additional logic needed for now
-  </script>
-  
-  <style scoped>
-  .video-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  </style>
+  <iframe 
+    class="video-container" 
+    src="/files/intro-28.mp4"
+    allowfullscreen
+    preload="none"
+    autoplay="false"
+    controls
+  ></iframe>
+</template>
+
+<script setup>
+// No additional logic needed for now
+</script>
+
+<style scoped>
+.video-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
